13946cdfee7eb13ad73114f576ff2f790b5f7eae
sumas7 <smlcurreli@gmail.com>
2024-03-02 19:23:02 +0100

    fuck state machine
    - state machine fucking sucks I disabled it (it used 13ms of time to process everything for some reason)
    - removed unused vs code files

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -2,34 +2,34 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	// [Export] public const float SPEED = 300.0f;
-	// [Export] public const float JUMP_STRENGTH = -400.0f;
+	[Export] public const float SPEED = 300.0f;
+	[Export] public const float JUMP_STRENGTH = -400.0f;
 
-	// public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	// public override void _PhysicsProcess(double delta)
-	// {
-	// 	if(Input.IsActionPressed("test"))
-    //         Engine.MaxFps = 20;
-    //     else
-    //         Engine.MaxFps = 60;
+	public override void _PhysicsProcess(double delta)
+	{
+		if(Input.IsActionPressed("test"))
+            Engine.MaxFps = 20;
+        else
+            Engine.MaxFps = 60;
 		
-	// 	Vector2 velocity = Velocity;
+		Vector2 velocity = Velocity;
 
-	// 	if(!IsOnFloor())
-	// 		velocity.Y += gravity * (float)delta;
+		if(!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
 
-	// 	// jump
-	// 	if(Input.IsActionJustPressed("jump") && IsOnFloor())
-	// 		velocity.Y = JUMP_STRENGTH;
+		// jump
+		if(Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JUMP_STRENGTH;
 
-	// 	Vector2 direction = Input.GetVector("left", "right", "up", "down");
+		Vector2 direction = Input.GetVector("left", "right", "up", "down");
 		
-	// 	velocity.X = direction.X * SPEED;
+		velocity.X = direction.X * SPEED;
 
-	// 	DebugUI.Instance.Text = velocity.ToString();
+		DebugUI.Instance.Text = velocity.ToString();
 
-	// 	Velocity = velocity;
-	// 	MoveAndSlide();
-	// }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
