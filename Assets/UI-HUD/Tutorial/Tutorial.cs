59b6339f89fe807987c288cdb70703f6001e7e65
sumas7 <smlcurreli@gmail.com>
2024-03-12 23:18:46 +0100

    Better tutorial layout
    - up button actually displayed upper than the others

--- Assets/UI-HUD/Tutorial/Tutorial.cs
+++ Assets/UI-HUD/Tutorial/Tutorial.cs
@@ -2,21 +2,23 @@ using Godot;
 
 public partial class Tutorial : CanvasLayer
 {
-	private RichTextLabel moveLabel, jumpLabel, dashLabel;
+	private RichTextLabel moveLabel, upLabel, jumpLabel, dashLabel;
 
 	public override void _Ready()
 	{
 		moveLabel = (RichTextLabel)GetChild(0);
-		jumpLabel = (RichTextLabel)GetChild(1);
-		dashLabel = (RichTextLabel)GetChild(2);
+		upLabel = (RichTextLabel)GetChild(1);
+		jumpLabel = (RichTextLabel)GetChild(2);
+		dashLabel = (RichTextLabel)GetChild(3);
 
 		moveLabel.Text += 
-			GameManager.Instance.currentControls[0].ToString() + " " +
-			GameManager.Instance.currentControls[2].ToString() + " " +
-			GameManager.Instance.currentControls[1].ToString() + " " +
-			GameManager.Instance.currentControls[3].ToString();
+			GameManager.Instance.currentControls[2].ToString() + " " + // left
+			GameManager.Instance.currentControls[1].ToString() + " " + // down
+			GameManager.Instance.currentControls[3].ToString(); // right
 			
-		jumpLabel.Text += GameManager.Instance.currentControls[4].ToString();
-		dashLabel.Text += GameManager.Instance.currentControls[5].ToString();
+		upLabel.Text += GameManager.Instance.currentControls[0].ToString();
+
+		jumpLabel.Text += GameManager.Instance.currentControls[4].ToString(); // jump
+		dashLabel.Text += GameManager.Instance.currentControls[5].ToString(); // dash
 	}
 }
